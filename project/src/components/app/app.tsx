9b2fc0afc55902eedadfb1e50ff778d39e334325
Aleksandr Chernobrov <alesandro83@mail.ru>
2023-03-11 00:30:28 +0300

    Добавил маршруты

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,11 +1,27 @@
 import MainPage from '../../pages/main-page/main-page';
+import Page404 from '../../pages/page404/page404';
+import Login from '../../pages/login/login';
+import Offer from '../../pages/offer/offer';
+
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 type AppScreenProps = {
   numberOffers: number;
 }
 
 function App({numberOffers}: AppScreenProps): JSX.Element {
-  return <MainPage numberOffers={numberOffers} />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage numberOffers={numberOffers} />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/offer/" element={<Offer />}>
+          <Route path=":id" element={<Offer />} />
+        </Route>
+        <Route path="*" element={<Page404 />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
